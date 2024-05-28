ab30f72de099e1632773f86627f2aad24e6040b2
Vukašin Stanković <vule.stankovic2@gmail.com>
2024-05-28 17:49:45 +0200

    Add Font Awesome and Bootstrap with basic nav

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">AUTOMARKET</RouterLink>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link active" to="vehicle">Cars</RouterLink>
+          </li>
+        </ul>
+        <span class="navbar-text">
+        Korisnik 123
+      </span>
+      </div>
+    </div>
+  </nav>
+  <RouterView />
+</template>
+
